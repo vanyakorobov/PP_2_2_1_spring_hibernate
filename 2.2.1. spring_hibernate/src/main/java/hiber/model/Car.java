e519135ef08ec2d041c55ea6ac24fa7b3a647cd9
Ivan <vanya.korobov@yandex.ru>
2024-05-04 20:20:02 +0300

    first commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,53 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "series")
+    private int series;
+
+    @Column(name = "model")
+    private String model;
+
+    @OneToOne
+    @JoinColumn(name = "user_id") // Указывает, что это поле является внешним ключом, связывающим сущность Car с сущностью User
+    private User user;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.series = series;
+        this.model = model;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getModel() {
+        return model;
+    }
+
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+}
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -2,6 +2,9 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,9 @@ public class UserServiceImp implements UserService {
 
    @Autowired
    private UserDao userDao;
+
+   @Autowired
+   private SessionFactory sessionFactory;
 
    @Transactional
    @Override
@@ -26,4 +32,14 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {
+      Session session = sessionFactory.getCurrentSession(); // Получаем текущую сессию
+      Query query = session.createQuery(
+              "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series");
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return (User) query.uniqueResult();
+   }
 }
